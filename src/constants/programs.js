8adb4c59c6e4cfeba801a12b2837c4b76ce63f12
Diksha Sharma <123383817+DikshaDhasmana@users.noreply.github.com>
2025-01-03 22:15:02 +0530

    Update programs.js

--- src/constants/programs.js
+++ src/constants/programs.js
@@ -2,12 +2,12 @@ export const programsList = [
 
     {
         name:"NS-HACKS",
-        description:"NS-HACKS is an exclusive hackathon for the NAMESPACE BPIT community, where members come together to work on innovative problem statements, turning their ideas into impactful solutions. Teams collaborate to create projects that not only showcase their technical and creative skills but also contribute to the growth of the community. The projects developed during NS-HACKS are recognized as official community projects of NAMESPACE BPIT and The NAMESPACE Community.",
+        description:"NS-HACKS is an exclusive hackathon for the members of NAMESPACE BPIT, where members come together to work on innovative problem statements, turning their ideas into impactful solutions. Teams collaborate to create projects that not only showcase their technical and creative skills but also contribute to the growth of the community. The projects developed during NS-HACKS are recognized as official community projects of NAMESPACE BPIT and The NAMESPACE Community.",
         image: "./img/internal-programs/nshack.jpeg",
     },
     {
         name: "AlgoRumble",
-        description:"AlgoRumble is nameSpace's bi-weekly Data Structures and Algorithms contest, an engaging event designed to test participants' problem-solving skills and algorithmic ability. The problems are set by the members themselves on a rotating basis while the others participate in it and try to solve in the minimum time. This program fosters the spirit of healthy competition among the members.",
+        description:"AlgoRumble is NAMESPACE BPIT's bi-weekly Data Structures and Algorithms contest, an engaging event designed to test participants' problem-solving skills and algorithmic ability. The problems are set by the members themselves on a rotating basis while the others participate in it and try to solve in the minimum time. This program fosters the spirit of healthy competition among the members.",
         image: "./img/internal-programs/algorumble.png",
     },
     {
@@ -17,7 +17,7 @@ export const programsList = [
     },
     {
         name: "TechTrek",
-        description:"TechTrek is a series of interactive sessions on various technology topics to enrich knowledge and skills taken by seniors of nameSpace. From cutting-edge advancements to practical applications, each session is carefully curated to enrich knowledge and expand horizons.",
+        description:"TechTrek is a series of interactive sessions on various technology topics to enrich knowledge and skills taken by seniors of NAMESPACE BPIT. From cutting-edge advancements to practical applications, each session is carefully curated to enrich knowledge and expand horizons.",
         image: "./img/internal-programs/techtrek.png"
     },
 ];
